8c16b60af27739a1de5dae4344ee35a0e7be9c7d
dsneddon00 <dsneddon00@gmail.com>
2022-05-11 12:10:03 -0600

    IO and Hashes

--- BasicFunctions/basics.rb
+++ BasicFunctions/basics.rb
@@ -129,6 +129,38 @@ def mergeR(leftArr, rightArr)
 
 end
 
+def userInputGoodies()    
+    print "What's your first name? "
+    first_name = gets.chomp
+    first_name.capitalize!
+
+    print "What's your last name? "
+    last_name = gets.chomp
+    last_name.capitalize!
+
+    print "What's your city? "
+    city = gets.chomp
+    city.capitalize!
+
+    print "What's your state? "
+    state = gets.chomp
+    state.upcase!
+
+    print "You are #{first_name} #{last_name} and live at #{city}, #{state}"
+    return
+end
+
+def histogram()
+    puts "Insert Text to be read into the histogram: "
+    text = gets.chomp
+    words = text.split(" ")
+    frequencies = Hash.new(0)
+    words.each { |word| frequencies[word] += 1 }
+    frequencies = frequencies.sort_by {|a, b| b }
+    frequencies.reverse!
+    frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+end
+
 
 def main()
     helloWorld()
@@ -154,6 +186,10 @@ def main()
     puts "\n"
     puts "Merge Sort"
     puts mergeSort([11,5,7,6,15])
+
+    #userInputGoodies()
+
+    histogram()
 
 
 end
